8f73e3e3059744dadc2e98ed0ad9debc28b82733
andimalik <andi.malik.notifications@gmail.com>
2016-09-15 14:21:25 +0700

    Use external properties file for mail configuration

--- src/main/java/com/andimalik/learn/App.java
+++ src/main/java/com/andimalik/learn/App.java
@@ -1,9 +1,11 @@
 package com.andimalik.learn;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 import javax.mail.Message;
-import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
@@ -16,29 +18,21 @@ import javax.mail.internet.MimeMessage;
  */
 public class App {
 	public static void main(String[] args) {
-		String mailHost = "smtp.host.tld";
-		String smtpPort = "465";
-		final String mailUsername = "user1@host.tld";
-		final String mailPassword = "user1password";
-
-		Properties properties = new Properties();
-		properties.setProperty("mail.smtp.host", mailHost);
-		properties.setProperty("mail.smtp.port", smtpPort);
-		properties.put("mail.smtp.socketFactory.port", smtpPort);
-		properties.put("mail.smtp.socketFactory.class",
-				"javax.net.ssl.SSLSocketFactory");
-		properties.put("mail.smtp.auth", "true");
-
-		Session session = Session.getDefaultInstance(properties,
-				new javax.mail.Authenticator() {
-					@Override
-					protected PasswordAuthentication getPasswordAuthentication() {
-						return new PasswordAuthentication(mailUsername,
-								mailPassword);
-					}
-				});
-
 		try {
+			Properties properties = App.loadMailConfigProperties();
+
+			final String mailUsername = properties.getProperty("mail.username");
+			final String mailPassword = properties.getProperty("mail.password");
+
+			Session session = Session.getDefaultInstance(properties,
+					new javax.mail.Authenticator() {
+						@Override
+						protected PasswordAuthentication getPasswordAuthentication() {
+							return new PasswordAuthentication(mailUsername,
+									mailPassword);
+						}
+					});
+
 			MimeMessage message = new MimeMessage(session);
 			message.setFrom(new InternetAddress(mailUsername));
 
@@ -51,14 +45,24 @@ public class App {
 			message.addRecipients(Message.RecipientType.BCC,
 					"user6@host.tld,user7@host.tld");
 
-			message.setSubject("Send e-Mail with JavaMail and JAF");
-			message.setContent("<h1>HTML Header</h1><p>HTML paragraph.</p>",
+			message.setSubject("A Subject");
+			message.setContent("<h1>A Title</h1><p>A paragraph.</p>",
 					"text/html");
 
 			Transport.send(message);
 			System.out.println("Message sent.");
-		} catch (MessagingException messagingException) {
-			messagingException.printStackTrace();
+		} catch (Exception exception) {
+			exception.printStackTrace();
 		}
+	}
+
+	private static Properties loadMailConfigProperties() throws IOException {
+		InputStream inputFile = new FileInputStream(
+				"src/main/resources/mail.cfg.properties");
+		Properties properties = new Properties();
+
+		properties.load(inputFile);
+
+		return properties;
 	}
 }
